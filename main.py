8461cd2e63a46a60490b1fac1bc885eb98f579ce
展慧东 <huidong.zhan@meditrusthealth.com>
2024-12-30 15:52:29 +0800

    app样例

---
+++ main.py
@@ -0,0 +1,30 @@
+# main.py
+import os
+import sys
+import uvicorn
+import time
+import yaml
+from fastapi import Request, FastAPI, Query
+from starlette.types import ASGIApp
+from starlette.responses import StreamingResponse
+import threading
+import logging as logger
+import CreateApp
+# app = FastAPI(title='simple-ai', description='简易ai的组件服务')
+app = CreateApp.App.createApp()
+
+def on_start_up():
+    # 加载组件
+    from src.app.core.business_logic import load_classes_from_components, load_config
+    app.components_data = load_classes_from_components()
+
+    # 加载app配置
+    app.app_config = load_config()
+
+if __name__ == '__main__':
+    try:
+        logger.info("开始启动服务======")
+        threading.Thread(target=on_start_up).start()
+        uvicorn.run(app, host='0.0.0.0', port=8899)
+    except Exception as e:
+        logger.exception("server start error:{}".format(e))

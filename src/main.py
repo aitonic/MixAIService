e754a5a94b8f0153cfb7a161376dd04d5c88a15e
aitonic <aitonicta@gmail.com>
2025-01-07 18:31:35 +0800

    add env code

--- src/main.py
+++ src/main.py
@@ -4,13 +4,16 @@ import sys
 import threading
 
 import uvicorn
+from dotenv import load_dotenv
 
 import src.create_app as create_app
 from src.utils.logger import logger
 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
+env_path = os.path.join(project_root, "config", ".env")
 
+load_dotenv(dotenv_path=env_path)
 
 # app = FastAPI(title='simple-ai', description='简易ai的组件服务')
 app = create_app.App.create_app()
@@ -37,6 +40,10 @@ if __name__ == "__main__":
     try:
         logger.info("开始启动服务======")
         threading.Thread(target=on_start_up).start()
-        uvicorn.run(app, host="127.0.0.1", port=8899)
+        
+        host = os.getenv("SERVER_HOST", "127.0.0.1")
+        port = int(os.getenv("SERVER_PORT", "8899"))
+       
+        uvicorn.run(app, host=host, port=port)
     except Exception as e:
         logger.exception(f"server start error:{e}")
